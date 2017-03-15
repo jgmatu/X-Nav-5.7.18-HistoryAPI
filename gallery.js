3e6e748877c5f7a0d3161683133d503b1b32a38c
Javier Gutierrez Maturana <fj.gutierrezs91@gmail.com>
2017-03-15 14:44:23 +0100

    Part one finalized

--- gallery.js
+++ gallery.js
@@ -1,43 +1,48 @@
+const SERVER = "http://localhost:8000/gallery/"
+
 function supports_history_api() {
-  return !!(window.history && history.pushState);
+        return !!(window.history && history.pushState);
 }
 
 function swapPhoto(href) {
-  var req = new XMLHttpRequest();
-  req.open("GET",
-           "http://gsyc.es/~grex/history_api/gallery/" +
-             href.split("/").pop(),
-           false);
-  req.send(null);
-  if (req.status == 200) {
-    document.getElementById("gallery").innerHTML = req.responseText;
-    setupHistoryClicks();
-    return true;
-  }
-  return false;
+        var req = new XMLHttpRequest();
+
+        req.open("GET", SERVER + href.split("/").pop(), false);
+        req.send(null);
+
+        console.log(SERVER + href.split("/").pop());
+
+        if (req.status == 200) {
+                document.getElementById("gallery").innerHTML = req.responseText;
+                setupHistoryClicks();
+                return true;
+        }
+        return false;
 }
 
 function addClicker(link) {
-  link.addEventListener("click", function(e) {
-    if (swapPhoto(link.href)) {
-      history.pushState(null, null, link.href);
-      e.preventDefault();
-    }
-  }, true);
+        link.addEventListener("click", function(e) {
+                if (swapPhoto(link.href)) {
+                        history.pushState(null, null, link.href);
+                        e.preventDefault();
+                }
+        }, true);
 }
 
 function setupHistoryClicks() {
-  addClicker(document.getElementById("photonext"));
-  addClicker(document.getElementById("photoprev"));
+        addClicker(document.getElementById("photonext"));
+        addClicker(document.getElementById("photoprev"));
 }
 
 window.onload = function() {
-  if (!supports_history_api()) { return; }
-  setupHistoryClicks();
-  window.setTimeout(function() {
-    window.addEventListener("popstate", function(e) {
-      swapPhoto(location.pathname);
-    }, false);
-  }, 1);
-}
+        if (!supports_history_api()) {
+                return;
+        }
 
+        setupHistoryClicks();
+        window.setTimeout(function() {
+                window.addEventListener("popstate", function(e) {
+                        swapPhoto(location.pathname);
+                }, false);
+        }, 1);
+}
